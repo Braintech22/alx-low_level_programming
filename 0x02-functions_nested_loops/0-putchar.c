94c602f5903dab3faae53611ad05db1d8f38b436
Braintech22 <datveryjimmy@gmail.com>
2022-04-12 12:55:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
-int main()
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
 {
-   char z[100] = "_putchar";
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-   printf("%s", z); // %s is format specifier
-
-   return 0;
+return (0);
 }
